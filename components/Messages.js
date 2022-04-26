2338a1e57d9a4232cbbaf1aa7d7268edc053740f
Lincoln Wasden <lincolndwasden@gmail.com>
2022-04-26 17:26:05 -0600

    live set

--- components/Messages.js
+++ components/Messages.js
@@ -3,9 +3,25 @@ import {useRef} from "react";
 import {ByMoralis, useMoralis, useMoralisQuery} from "react-moralis"
 import SendMessage from "../components/SendMessage"
 
+const MINS_DURATION = 10000000000;
+
 function Messages() {
     const {user} = useMoralis();
     const endOfMessagesRef = useRef(null);
+    const {data, loading, error} = useMoralisQuery(
+        'Messages', 
+        (query) => 
+        query
+        .ascending('createdAt')
+        .greaterThan(
+            'creaetedAt', 
+            new Date(Date.now() -1000 * 60 * MINS_DURATION)
+            ),
+            [,
+            {
+                live: true,
+            }]
+    );
   return (
     <div className="pb-56">
         <div className="my-5">
