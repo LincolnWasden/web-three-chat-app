5ecb9adff40bae88138aa904b962e359c60546f5
Lincoln Wasden <lincolndwasden@gmail.com>
2022-04-26 16:26:34 -0600

    major commit

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,35 @@
+import { useMoralis } from "react-moralis"
+import Image from "next/image"
+import Avatar from "../components/Avatar"
+import ChangeUsername from "../components/ChangeUsername"
+
 function Header() {
+    const {user} = useMoralis();
   return (
-    <div>
-        <h1>I am a header</h1>
+    <div className="sticky top-0 p-5 z-50  text-pink-500">
+       <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
+           <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
+               <Image 
+               layout="fill" 
+               objectFit="cover"
+               className="rounded-full" 
+               src="/stat.jpg"/>
+           </div>
+           <div className="col-span-4 text-left lg:text-center">
+             <div className="relative h-48 w-48
+             lg:mx-auto border-pink-500 border-8 rounded-full">{/*Avatar*/}
+             
+             <Avatar logoutOnPress/>
+             
+             </div>  
+                {/*welcome message*/}
+                <h1 className="text-3xl">Welcome to the LinxHype Fam MetaVerse</h1>
+                <h2 className="text-5xl font-bold truncate">{user.getUsername()}</h2>
+              
+                  {/*Change username component*/}
+                  <ChangeUsername />
+           </div>
+       </div>
     </div>
   )
 }
