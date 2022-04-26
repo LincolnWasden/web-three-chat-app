c3291635ff1eac38dba982a8836fb03e108c682c
Lincoln Wasden <lincolndwasden@gmail.com>
2022-04-26 17:47:06 -0600

    message blue

--- components/Message.js
+++ components/Message.js
@@ -2,9 +2,22 @@ import {useMoralis} from "react-moralis";
 
 function Message({message}) {
     const { user } = useMoralis();
+    const isUserMessage = message.get('ethAddress') === user.get('ethAddress');
+
   return (
-    <div>
+    <div className={`flex items-end space-x-2 relative 
+    ${isUserMessage && "justify-end"}`}>
+        <div 
+        className={`flex space-x-4 p-3 rounded-lg ${
+        isUserMessage 
+        ? "rounded-br-none bg-blue-400"
+        : "rounded-bl-none bg-pink-300"
+        
+        
+        } `}
+        >
         <p>{message.get('message')}</p>
+        </div>
     </div>
   )
 }
